7f7fc474bd5aebb7b1ff2f65dc89a80ad9a387b8
jessicaagarwal <jessica.agarwal.s64@kalvium.community>
2025-02-07 09:33:27 +0530

    milestone 12

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from "react-router-dom"; 
-import { LoginPage, SignupPage, Home, CreateProduct} from "./Routes.jsx"
+import { LoginPage, SignupPage, Home, CreateProduct, MyProducts} from "./Routes.jsx"
 
 import './App.css';
 
@@ -12,6 +12,7 @@ function App(){
         <Route path="/Signup" element={<SignupPage/>}/>
         <Route path="/Home" element={<Home/>}/>
         <Route path='/create-product' element={<CreateProduct />} />
+        <Route path="/my-products" element={<MyProducts/>}/>
       </Routes>
     </BrowserRouter>
   )
--- frontEnd/src/Routes.jsx
+++ frontEnd/src/Routes.jsx
@@ -1,5 +1,6 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import Home from "./pages/Home";
-import CreateProduct from './pages/createProduct'  
-export {LoginPage, SignupPage,Home,CreateProduct};
+import CreateProduct from './pages/createProduct'
+import MyProducts from "./pages/myProducts";
+export {LoginPage, SignupPage,Home,CreateProduct, MyProducts};
