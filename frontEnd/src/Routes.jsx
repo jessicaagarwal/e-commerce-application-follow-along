08240fa6db58d8d3b400da7d5c13365672d7ac10
jessicaagarwal <jessica.agarwal.s64@kalvium.community>
2025-02-28 13:38:14 +0530

    milestone 24

--- frontEnd/src/Routes.jsx
+++ frontEnd/src/Routes.jsx
@@ -8,4 +8,5 @@ import ProductDetails from "./pages/productDetails";
 import Profile from "./pages/profile";
 import CreateAddress from "./pages/createAddress";
 import SelectAddress from "./pages/SelectAddress";
-export {LoginPage, SignupPage,Home,CreateProduct, MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress};
+import OrderConfirmation from "./pages/OrderConfirmation";
+export {LoginPage, SignupPage,Home,CreateProduct, MyProducts,Cart,ProductDetails,Profile,CreateAddress,SelectAddress,OrderConfirmation};
